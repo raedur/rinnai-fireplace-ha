486de1c22a4370449344c2eae5d1b0269278b3de
Raedur <readur@gmail.com>
2024-09-12 05:01:46 +0000

    add config url to get ip address

--- custom_components/rinnai_fireplace/entity.py
+++ custom_components/rinnai_fireplace/entity.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import MANUFACTURER
+from .const import CONF_IP, MANUFACTURER
 from .coordinator import RinnaiFireplaceDataUpdateCoordinator
 
 
@@ -22,6 +22,7 @@ class RinnaiFireplaceEntity(CoordinatorEntity[RinnaiFireplaceDataUpdateCoordinat
 
     @property
     def device_info(self) -> DeviceInfo:
+        """Return the device info."""
         return DeviceInfo(
             identifiers={
                 (
@@ -29,6 +30,7 @@ class RinnaiFireplaceEntity(CoordinatorEntity[RinnaiFireplaceDataUpdateCoordinat
                     self.coordinator.config_entry.entry_id,
                 ),
             },
+            configuration_url=f"http://{self.coordinator.config_entry.data[CONF_IP]}:3000",
             manufacturer=MANUFACTURER,
             name=self.coordinator.device_name,
             sw_version=self.coordinator.sw_version,
